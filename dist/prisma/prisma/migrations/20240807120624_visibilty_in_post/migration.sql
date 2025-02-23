45591ee8ffb9fcac3073b8aaa4dfb00dd013ee5f
pankaaaaa <pankajsuthar9610@gmail.com>
2025-02-23 14:14:38 +0530

    5

--- dist/prisma/prisma/migrations/20240807120624_visibilty_in_post/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "VisibilityType" AS ENUM ('ONLY_FOLLOWING', 'PUBLIC');
-
--- AlterTable
-ALTER TABLE "Post" ADD COLUMN     "visibility" "VisibilityType" DEFAULT 'ONLY_FOLLOWING';
